1ad1526ec32eb297a6680147b98fe3f345a15a16
Qi Shi <qi.shi@clogin06.cactus.wcoss2.ncep.noaa.gov>
2025-09-16 14:54:18 +0000

    add aigfs to verification

---
+++ dev/drivers/scripts/stats/global_det/jevs_global_det_atmos_aigfs_grid2obs_stats.sh
@@ -0,0 +1,60 @@
+#PBS -N jevs_global_det_atmos_aigfs_grid2obs_stats_00
+#PBS -j oe
+#PBS -S /bin/bash
+#PBS -q dev
+#PBS -A VERF-DEV
+#PBS -l walltime=01:30:00
+#PBS -l place=vscatter:exclhost,select=1:ncpus=128:ompthreads=1:mem=250GB
+#PBS -l debug=true
+#PBS -V
+
+set -x
+
+cd $PBS_O_WORKDIR
+
+export model=evs
+export HOMEevs=/lfs/h2/emc/vpppg/noscrub/qi.shi/EVS_graphcastGFS/EVS
+
+export SENDCOM=YES
+export SENDMAIL=NO
+export KEEPDATA=NO
+export job=${PBS_JOBNAME:-jevs_global_det_atmos_aigfs_grid2obs_stats}
+export jobid=$job.${PBS_JOBID:-$$}
+export SITE=$(cat /etc/cluster_name)
+export vhr=00
+
+source $HOMEevs/versions/run.ver
+module reset
+module load prod_envir/${prod_envir_ver}
+source $HOMEevs/dev/modulefiles/global_det/global_det_stats.sh
+
+evs_ver_2d=$(echo $evs_ver | cut -d'.' -f1-2)
+
+export machine=WCOSS2
+export USE_CFP=YES
+export nproc=128
+
+export MAILTO='qi.shi@noaa.gov'
+
+export envir=prod
+export NET=evs
+export STEP=stats
+export COMPONENT=global_det
+export RUN=atmos
+export VERIF_CASE=grid2obs
+export MODELNAME=aigfs
+
+export DATAROOT=/lfs/h2/emc/stmp/$USER/evs_test/$envir/tmp
+export TMPDIR=$DATAROOT
+export COMIN=/lfs/h2/emc/vpppg/noscrub/qi.shi/EVS_graphcastGFS/$NET/$evs_ver_2d
+export COMOUT=/lfs/h2/emc/vpppg/noscrub/qi.shi/EVS_graphcastGFS/$NET/$evs_ver_2d/$STEP/$COMPONENT
+
+export config=$HOMEevs/parm/evs_config/global_det/config.evs.${STEP}.${COMPONENT}.${RUN}.${VERIF_CASE}.aigfs
+
+# CALL executable job script here
+$HOMEevs/jobs/JEVS_GLOBAL_DET_STATS
+
+######################################################################
+# Purpose: This does the statistics work for the global deterministic
+#          atmospheric grid-to-observations component for GraphcastGFS - 13 levels
+######################################################################
